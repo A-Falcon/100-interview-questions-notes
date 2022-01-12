c7c58ebef7840750ae0b6a97d859fe9cb7e1c4da
Ahtziri <Afalcon1209@gmail.com>
2022-01-12 15:12:14 -0800

    add const let var notes

---
+++ js/constLetVar.js
@@ -0,0 +1,14 @@
+// const
+// introduces in es6
+// block scope - only exists in that scope
+// unchanging values (constants)
+
+// let
+// introduced in es6
+// block scope - only exists in that scope
+// reassignment value
+
+// var
+// lexical scope
+// caused variable hoisting
+// very buggy code
