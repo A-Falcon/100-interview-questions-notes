065456768612f67816cf789cf6317c1c0c00d74c
Ahtziri <Afalcon1209@gmail.com>
2022-01-12 15:04:21 -0800

    add js datatypes

---
+++ js/dataTypes.js
@@ -0,0 +1,13 @@
+// there are several datatypes in javaScript.
+//some of them are primitives :
+//Boolean - true false
+//numbers (whole numbers, floating point numbers)
+//null - empty value that a user has assigned
+//undefined - no vale
+//strings - 'hello'
+
+//non-primitives
+//objects
+//arrays
+//maps - contains datas in key-value pair
+//sets - unordered and contains different elements
