259aafd811cc4fdfdb8bea54eedeb22f5f13d02c
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 12:06:04 -0800

    add ref vs val

---
+++ js/refrenceVsValue.js
@@ -0,0 +1,11 @@
+// passing a refrence by value
+// you make a copy of the parameter
+// function gets a copy of the variable
+// and changes are not reflected to the original value
+// takes up more memory
+
+//passing by refrence
+//you pass the address of the parameter
+//function access the variables content
+//changes are reflected to the original value
+//takes less memory
