e5cd0b77fa0adba04d098f1bba0c16ffb6517b3e
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 12:40:16 -0800

    add double vs tripple equal

---
+++ js/doubleVsTriple.js
@@ -0,0 +1,5 @@
+// == is used when you want to equal the value
+//ex : '1' == 1 is true
+
+// === is used when you want to be more strict and it will be strict with the data type
+// ex: '1' === 1 is false since they are not the same data type.
