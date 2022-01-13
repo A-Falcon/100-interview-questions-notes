7fae3234bda19d9d5caf87102806edf84be870e2
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 15:44:44 -0800

    add coercion

---
+++ js/coercion.js
@@ -0,0 +1,8 @@
+//is when you add the different data types(string and nubmer) together and the number gets 'eaten' by the string.
+
+const total = 2 + 3 + '7'
+console.log(total)
+console.log(typeof total)
+console.log(typeof String(5))
+console.log(typeof Boolean(0))
+console.log(typeof Number('5'))
