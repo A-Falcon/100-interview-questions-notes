e600bcb609c5d383fb8b7166b86d510bce491e90
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 12:28:19 -0800

    add falsey values

---
+++ js/falseyValues.js
@@ -0,0 +1,21 @@
+// falsey come in many forms
+// Boolean False
+// undefined
+// null
+// empty string
+// 0 number is false
+// Nan (not a number)
+
+const example1 = undefined
+const example2 = null
+const example3 = NaN
+const example4 = 0
+const example5 = ''
+const example6 = false
+
+console.log(!!example1)
+console.log(!!example2)
+console.log(!!example3)
+console.log(!!example4)
+console.log(!!example5)
+console.log(!!example6)
