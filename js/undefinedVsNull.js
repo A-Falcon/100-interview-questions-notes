e9eac988e5ab3212e6db0dd5f2f5476c44dc119c
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 17:17:03 -0800

    add undefined vs null

---
+++ js/undefinedVsNull.js
@@ -0,0 +1,11 @@
+// undefined : variable has NOT been assigned a value
+//null: variable has YET to have a value
+
+const user1 = {
+
+}
+let user2 = null
+
+console.log(user1)
+console.log(user1.firstName)
+console.log(user3)
