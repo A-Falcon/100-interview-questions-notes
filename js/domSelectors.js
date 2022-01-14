a646896ae66b41dec11b2fcd956ba80940564772
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 18:21:31 -0800

    add dom selectors

---
+++ js/domSelectors.js
@@ -0,0 +1,18 @@
+//manipulating the DOM is one of the most cruitial parts of be a developer
+//for main eay to selesct element in the dom
+
+//.getElementById() will return a single result
+
+//.getElementByTag() we could get multiple tags (h1, div, p, etc)
+
+//css selector, you can match all the elements that woulc match the  css seeslctor
+
+//classname,
+
+//vanilla js 
+
+console.log(document.getElementById('id-example'),
+console.log(document.getElementById('id-example').innerHTML = 'Hello'))
+console.log(document.getElementsByTagName('h1'))
+console.log(document.getElementsByClassName('some-class'))
+console.log(document.querySelector('body'))// slow asf
