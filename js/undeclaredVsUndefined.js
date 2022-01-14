f4df8cc074ec296cbcaf5a52f21c67ae8e0237a0
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 18:13:55 -0800

    add undeclared vsundefined

---
+++ js/undeclaredVsUndefined.js
@@ -0,0 +1,12 @@
+// undeclared is when a value does not exsist and your trying to access it
+//undefined is when the variable exists but has no value
+
+const user = {
+
+}
+
+let example 
+
+console.log(user.firstNmae)
+console.log(example)
+console.log(example2)
