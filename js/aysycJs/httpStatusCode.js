4d38070b89158936f532ddd4104e894d71ff5f00
Ahtziri <Afalcon1209@gmail.com>
2022-01-14 12:19:08 -0800

    add http status codes

---
+++ js/aysycJs/httpStatusCode.js
@@ -0,0 +1,16 @@
+//http status codes
+// 5 main groupings
+//100 , 200 , 300 , 400 , 500
+
+//100 level status codes are informational status codes
+
+//200 level status codes its working, it is successfull
+
+//300 status codes - someone is trying to acess something they shouldnt and we redirect them
+
+//400 status codes - problems on the client side
+    //401 - theyre unauthorized
+    //404 - trying to access something that does not exsist
+
+
+//500 status code - server level error
