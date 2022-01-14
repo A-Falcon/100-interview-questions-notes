9c6f1a0bdc69917b4ff24712b561e475ea8c560e
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 16:22:50 -0800

    add strictMode

---
+++ js/strictMode.js
@@ -0,0 +1,6 @@
+// strict mode exists so that there are better coding practices and less bugs
+// strict mode will enabel:
+// no global variables
+// no deleting keys
+// no duplicating  parameters
+
