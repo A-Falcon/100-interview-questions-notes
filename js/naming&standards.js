44892e460ebb343f55b742b54471b7c9d58a965b
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 16:53:53 -0800

    add naming and standards

---
+++ js/naming&standards.js
@@ -0,0 +1,20 @@
+//coding ettiquite
+//naming conventions
+
+
+//booleans: try to use the IS HAS prefix because it reads cleaner
+
+//function calls that return something: prefix the name with a verb
+//avoid 'hungarian notation' (outdated practice) which is putting bool or num in their variables
+// now wwe have typescript so its no necessary
+
+//css coding principles
+// have few as few selectors as possible so that css cascades nicly
+
+
+//do not have variable names in all caps
+
+// have less variables as possible to avoid clutter
+
+//avoid 'magic numbers' (numbers came out of no where)
+//macke sure to declare your magic nubmers
