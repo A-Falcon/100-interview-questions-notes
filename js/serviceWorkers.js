a12bd5a64a6087ae10631ec013481c759f8a2bae
Ahtziri <Afalcon1209@gmail.com>
2022-01-13 18:41:00 -0800

    add service workers

---
+++ js/serviceWorkers.js
@@ -0,0 +1,9 @@
+//are a major part of pwa (progressive web applications) 
+//provide a lot of functionality
+//catch a lot of resource before they fire off
+//assets that wont immediatly or very often change like fonts 
+//you might catch your api calls
+
+//benefits: 
+// if our app loses internet connect we can have offline mode
+
