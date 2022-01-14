2a090739d2067ee07a9fe2569d84203465daa5b5
Ahtziri <Afalcon1209@gmail.com>
2022-01-14 12:24:11 -0800

    add REST standards

---
+++ js/aysycJs/restStandards.js
@@ -0,0 +1,6 @@
+// REST - series of architechure patterns to help build apis
+
+//versioning - youll have to update your API's  if there are breaking changes
+
+//nouns- use words that make sense so tht theyre readable 
+//ex: getUser()
